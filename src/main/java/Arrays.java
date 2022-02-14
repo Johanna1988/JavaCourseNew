fdd00c648244743b2f53ff50f09f09e5ac82d6c0
Yana Chorna <johanna1988@ukr.net>
2022-02-14 22:16:41 +0300

    fixed mistakes

--- src/main/java/Arrays.java
+++ src/main/java/Arrays.java
@@ -6,15 +6,16 @@ public class Arrays {
         for (int i = 0; i < numbers.length; ++i) {
             numbers[i] = i * 10;
         }
+
         for (int i = 0; i < numbers.length; ++i) {
             System.out.println(numbers[i]);
         }
+
         System.out.println();
         int[] numbers1 = new int[]{1, 2, 3};
 
         for (int i = 0; i < numbers1.length; ++i) {
             System.out.println(numbers1[i]);
-
         }
     }
 }
