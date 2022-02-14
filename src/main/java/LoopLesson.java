3bbb304388e98ceeb9c788ae7762fb3c5424fb5d
Johanna1988 <98016306+Johanna1988@users.noreply.github.com>
2022-02-14 22:22:38 +0300

    new version (#1)
    * Loop Lesson was done
    Co-authored-by: Yana Chorna <johanna1988@ukr.net>

---
+++ src/main/java/LoopLesson.java
@@ -0,0 +1,9 @@
+package main.java;
+
+public class LoopLesson {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i = i + 1) {
+            System.out.println("Hello");
+        }
+    }
+}
