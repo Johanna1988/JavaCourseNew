f15208933b6c013d1f7668e00ad72eebac57d368
Yana Chorna <johanna1988@ukr.net>
2022-02-14 21:18:43 +0300

    version 2.0

--- src/main/java/LoopLesson.java
+++ src/main/java/LoopLesson.java
@@ -1,6 +1,8 @@
+package main.java;
+
 public class LoopLesson {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i = i + 1) {
+        for (int i = 0; i < 5; i = i + 1) {
             System.out.println("Hello");
         }
     }
