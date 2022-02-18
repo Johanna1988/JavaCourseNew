7c40bf56d2c16574446dadaeee54f4ff93474fc9
Johanna1988 <98016306+Johanna1988@users.noreply.github.com>
2022-02-19 00:41:36 +0300

    Calculator (#3)
    * Created calculator
    
    Co-authored-by: Yana Chorna <johanna1988@ukr.net>

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,41 @@
+package main.java;
+import java.util.Scanner;
+public class Calculator {
+
+    public static void main(String[] args) {
+        int number1;
+        int number2;
+        int result;
+        char action;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter first number");
+        number1 = input.nextInt();
+        System.out.println("Please provide an action: /, *, -, + ");
+        action = input.next().charAt(0);
+        System.out.println("Please enter second number");
+        number2 = input.nextInt();
+        switch (action) {
+            case '/':
+                result = number1 / number2;
+                System.out.println("The result is: ");
+                System.out.println(number1 + " / " + number2 + " = " + result);
+                break;
+            case '*':
+                result = number1 * number2;
+                System.out.println("The result is: ");
+                System.out.println(number1 + " * " + number2 + " = " + result);
+                break;
+            case '-':
+                result = number1 - number2;
+                System.out.println("The result is: ");
+                System.out.println(number1 + " - " + number2 + " = " + result);
+                break;
+            case '+':
+                result = number1 + number2;
+                System.out.println("The result is: ");
+                System.out.println(number1 + " + " + number2 + " = " + result);
+                break;
+
+        }
+    }
+}
