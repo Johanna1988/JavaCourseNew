2bb98610d1108cb67cee90b5fd5f296c1e0f45a7
Yana Chorna <johanna1988@ukr.net>
2022-05-28 20:04:15 +0300

    Deleted comments

--- src/CursorLecture1.java
+++ src/CursorLecture1.java
@@ -3,9 +3,6 @@
 public class CursorLecture1 {
 
     public static void main(String[] args) {
-       // System.out.println(5 + 5);
-        // System.out.println("5+5");
-
 
         getText("hello");
         printNumber();
