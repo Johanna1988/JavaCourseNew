1397a61c07999267f1d8e725e0f9bed3ccae70b5
Yana Chorna <johanna1988@ukr.net>
2022-05-28 19:54:42 +0300

    Added code from lecture 1

---
+++ src/CursorLecture1.java
@@ -0,0 +1,26 @@
+
+
+public class CursorLecture1 {
+
+    public static void main(String[] args) {
+       // System.out.println(5 + 5);
+        // System.out.println("5+5");
+
+
+        getText("hello");
+        printNumber();
+    }
+
+    static void getText(String s) {
+        System.out.println(s);
+    }
+    static void printNumber() {
+        System.out.println(15);
+
+        String a = "Hello";
+        int b = 15;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
+
