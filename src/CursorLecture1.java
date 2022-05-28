c14d4b8f7c34e086efa5fdeb5bb219b8fd1950bf
Johanna1988 <98016306+Johanna1988@users.noreply.github.com>
2022-05-28 20:07:05 +0300

    Calculator (#4)
    * new lesson about arrays
    
    * Created calculator
    
    * Added code from lecture 1
    
    Co-authored-by: Yana Chorna <johanna1988@ukr.net>

---
+++ src/CursorLecture1.java
@@ -0,0 +1,23 @@
+
+
+public class CursorLecture1 {
+
+    public static void main(String[] args) {
+
+        getText("hello");
+        printNumber();
+    }
+
+    static void getText(String s) {
+        System.out.println(s);
+    }
+    static void printNumber() {
+        System.out.println(15);
+
+        String a = "Hello";
+        int b = 15;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
+
